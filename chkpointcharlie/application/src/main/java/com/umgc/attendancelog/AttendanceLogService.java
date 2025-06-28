f3d6cd985494bd3724a8a939d2b22399df331b9c
hmant <hmant@host.docker.internal>
2025-06-28 17:05:53 -0400

    Added Attendance Log Serive Layer

--- chkpointcharlie/application/src/main/java/com/umgc/attendancelog/AttendanceLogService.java
+++ chkpointcharlie/application/src/main/java/com/umgc/attendancelog/AttendanceLogService.java
@@ -4,14 +4,23 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableConfigurationProperties(ServiceProperties.class)
 public class AttendanceLogService {
 
+
+	private final ServiceProperties serviceProperties;
+	
+	public AttendanceLogService(ServiceProperties serviceProperties) {
+	    this.serviceProperties = serviceProperties;
+	}
+	
     @Autowired
     private AttendanceLogRepository attendanceLogRepository;
-
+    
     public List<AttendanceLog> findAll() {
         return attendanceLogRepository.findAll();
     }
@@ -27,6 +36,10 @@ public class AttendanceLogService {
     public void deleteById(Long id) {
     	attendanceLogRepository.deleteById(id);
     }
+    
+    public String message() {
+		return this.serviceProperties.getMessage();
+	}
+}
 
    
-}
